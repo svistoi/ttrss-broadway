adc853b716e711130b9bf6d83793d7bd465de1ba
Igor Svistoun <igor.svistoun@protonmail.com>
2020-05-26 17:54:44 -0400

    bugfix: article_id was not unique per user and api

--- lib/broadway/article_message.ex
+++ lib/broadway/article_message.ex
@@ -1,22 +1,25 @@
 defmodule ArticleMessage do
   @enforce_keys [:article_id, :api_url, :sid, :output_dir, :article]
-  defstruct article_id: nil,
-            api_url: nil,
-            sid: nil,
-            output_dir: nil,
-            download_url: nil,
-            article: %{}
+  defstruct article_id: nil,    # calculated unique article id
+            api_url: nil,       # tt-rss url where this article came from
+            sid: nil,           # authentication SID against the api_url
+            username: nil,      # username used to login to the API
+            output_dir: nil,    # target where this article is going
+            download_url: nil,  # post-processed article map
+            article: %{}        # raw article from the fee
 
-  def new(article = %{}, api_url, output_dir, sid) do
+
+  def new(article = %{}, api_url, username, output_dir, sid) do
     # Because we could be connecting to multiple endpoints, the unique article
     # ID needs to be in context of api endpoint; ID 1 on tt-rss A, may be a
     # different article on tt-rss B
     # TODO: see tt-rss article guid definition, looks like an alternative
-    article_id = "#{Map.get(article, "id", "no_id")}#{api_url}"
+    article_id = "#{Map.get(article, "id", "no_id")}#{api_url}#{username}"
     %ArticleMessage{
       article_id: article_id,
       api_url: api_url,
       sid: sid,
+      username: username,
       output_dir: output_dir,
       article: Map.drop(article, ["content", "comments"])
     }
--- lib/util/unread_article_fetch.ex
+++ lib/util/unread_article_fetch.ex
@@ -3,8 +3,28 @@ defmodule Util.UnreadArticleFetch do
   # Pipeline
   use GenServer
   require Logger
-
   @defaults [interval: 10_000]
+
+  defmodule Account do
+    @enforce_keys [:api_url, :username, :password, :output_dir]
+    defstruct api_url: nil,
+              username: nil,
+              password: nil,
+              output_dir: nil,
+              sid: nil
+
+    def new!(account = %{}) do
+      account = %Account{
+        api_url: Map.fetch!(account, "api"),
+        username: Map.fetch!(account, "username"),
+        password: Map.fetch!(account, "password"),
+        output_dir: Map.fetch!(account, "output")
+      }
+
+      {:ok, sid} = TTRSS.Client.login(account.api_url, account.username, account.password)
+      %Account{account | sid: sid}
+    end
+  end
 
   def start_link(opts) do
     opts = Keyword.merge(@defaults, opts)
@@ -22,18 +42,14 @@ defmodule Util.UnreadArticleFetch do
     end
   end
 
-  def trigger_update() do
-    GenServer.cast(__MODULE__, {:update})
-  end
-
   @impl true
   def handle_info(:tick, state) do
-    trigger_update()
+    GenServer.cast(__MODULE__, {:update})
     {:noreply, state}
   end
 
   @impl true
-  def handle_cast({:update}, accounts) do
+  def handle_cast({:update}, accounts = [%Account{}]) do
     Logger.debug("Cron job looking up unread articles")
 
     unread_articles =
@@ -47,33 +63,24 @@ defmodule Util.UnreadArticleFetch do
     Logger.debug(
       "Pushing #{length(unread_articles)} articles to Broadway Producer #{inspect(producer_name)}"
     )
-
     GenStage.cast(producer_name, {:notify, unread_articles})
 
     {:noreply, accounts}
   end
 
-  defp authenticate_accounts(accounts) do
+  defp authenticate_accounts(accounts = [%{}]) do
     accounts
-    |> Enum.map(fn account ->
-      api_url = Map.fetch!(account, "api")
-      username = Map.fetch!(account, "username")
-      password = Map.fetch!(account, "password")
-      {:ok, sid} = TTRSS.Client.login(api_url, username, password)
-      Map.put(account, "sid", sid)
-    end)
+    |> Enum.map(&Account.new!(&1))
   end
 
   # Fetches article given account and returns them via
-  defp get_unread_article_messages(account) do
-    output_dir = Map.fetch!(account, "output")
-    api_url = Map.fetch!(account, "api")
-    sid = Map.fetch!(account, "sid")
-    Logger.debug("Getting articles for #{api_url} destined for #{output_dir}")
-
-    {:ok, unread_articles} = TTRSS.Client.get_all_unread_articles(api_url, sid)
+  defp get_unread_article_messages(account = %Account{}) do
+    Logger.debug("Getting articles for #{account.api_url} destined for #{account.output_dir}")
+    {:ok, unread_articles} = TTRSS.Client.get_all_unread_articles(account.api_url, account.sid)
 
     unread_articles
-    |> Enum.map(&ArticleMessage.new(&1, api_url, output_dir, sid))
+    |> Enum.map(
+      &ArticleMessage.new(&1, account.api_url, account.username, account.output_dir, account.sid)
+    )
   end
 end
