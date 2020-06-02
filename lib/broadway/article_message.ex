3f4f76bd26fecdab86bfb7a26d0d47a50765b8d8
Igor Svistoun <igor.svistoun@protonmail.com>
2020-06-01 22:01:44 -0400

    refactored article_id to re-use same composite key

--- lib/broadway/article_message.ex
+++ lib/broadway/article_message.ex
@@ -2,7 +2,7 @@ defmodule Broadway.ArticleMessage do
   alias __MODULE__
 
   @enforce_keys [:article_id, :account]
-  # For hashing purposes (keeping track of messages sent out for processing) article_id is calculated
+  # For hashing purposes (keeping track of messages sent out for processing) composite key
   defstruct article_id: nil,
             # account structure this article came from
             account: nil,
@@ -15,8 +15,15 @@ defmodule Broadway.ArticleMessage do
     # Because we could be connecting to multiple endpoints, the unique article
     # ID needs to be in context of api endpoint; ID 1 on tt-rss A, may be a
     # different article on tt-rss B
-    # TODO: see tt-rss article guid definition, looks like an alternative
-    article_id = "#{Map.get(article, "id", "no_id")}#{account.api_url}#{account.username}"
+    #
+    # Lots of options how to calculate the composite key include "id" from tt-rss
+    #
+    # Article title, shouldn't use id because on article re-publish the id will
+    # be different
+    title = Map.get(message.article, "title", "")
+    # feed id, not feed_title, I can rename the feed
+    feed_id = Map.get(message.article, "feed_id", "")
+    article_id = "#{title}-#{feed_id}-#{message.account.api_url}-#{message.account.username}"
 
     %ArticleMessage{
       article_id: article_id,
